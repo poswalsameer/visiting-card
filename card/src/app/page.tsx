3b6b3fd96ec4ef07d22dff5c3219a13840c091e1
poswalsameer <poswalsameer2003@gmail.com>
2024-10-10 23:58:52 +0530

    basic layout is complete

--- card/src/app/page.tsx
+++ card/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-
 import { Tilt } from "react-tilt";
+import { Github, Linkedin, Twitter, Globe } from 'lucide-react';
+
 
 export default function Home() {
   const defaultOptions = {
@@ -21,8 +22,112 @@ export default function Home() {
         options={defaultOptions}
         className="flex justify-center items-center h-[80%] w-[80%]"
       >
-        <div className="h-[80%] w-[80%] bg-black border-2 border-gray-500 rounded-2xl shadow-2xl flex justify-center items-center">
-          {/* Content inside Tilt */}
+        <div className="h-[80%] w-[80%] bg-black rounded-2xl shadow-2xl flex justify-center items-center">
+
+          {/* Left side wala div */}
+          <div className="h-full w-[30%] flex flex-col justify-center items-center" >
+
+            <div className="h-[30%] w-full flex justify-center items-center  " >
+              <img src="./profile.jpg" alt="" className="h-44 w-44 rounded-full" />
+            </div>
+
+            <div className="h-[30%] w-full  flex gap-x-5 justify-center items-center" >
+              < Github className="text-white h-7 w-7"/>
+              < Linkedin className="text-white h-7 w-7"/>
+              < Twitter className="text-white h-7 w-7"/>
+              < Globe className="text-white h-7 w-7"/>
+            </div>
+
+          </div>
+
+          {/* Right side wala div */}
+          <div className="h-full w-[70%] flex flex-col gap-y-0 justify-center items-center" >
+
+            <div className="h-[45%] w-full flex flex-col justify-center items-center text-white" >
+              <div className=" text-7xl text-center w-full flex justify-center items-center" >SAMEER POSWAL</div>
+              <div className="h-[20%] w-full text-lg flex justify-center items-center" > Software Engineer • Full-Stack Engineer • Open Source Contributor </div>
+            </div>
+
+            <div className=" h-[30%] w-full text-white flex gap-x-3 justify-center items-center " >
+              
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+            </div>
+
+            <div className="h-[25%] w-full" >
+
+            </div>
+
+          </div>
+
+
+          {/* Profile photo and name wala div */}
+          {/* <div className=" h-[40%] w-full flex flex-row justify-center items-center " >
+
+            <div className="h-full w-[30%] flex justify-center items-center  " >
+              <img src="./profile.jpg" alt="" className="h-44 w-44 my-20 rounded-full" />
+            </div>
+
+            <div className="h-full w-[80%] flex flex-col justify-center items-center text-white" >
+              <div className=" text-7xl text-center w-full flex justify-center items-center" >SAMEER POSWAL</div>
+              <div className="h-[25%] w-full text-lg flex justify-center items-center" > Software Engineer • Full-Stack Engineer • Open Source Contributor </div>
+            </div>
+            
+          </div> */}
+
+          {/* Social Profiles + ( Role + Recent work ) wala div */}
+          {/* <div className=" h-[30%] w-full flex flex-row justify-center items-center " >
+
+           
+            <div className=" h-[50%] w-[30%] border flex gap-x-5 justify-center items-center" >
+
+              < Github className="text-white h-7 w-7"/>
+              < Linkedin className="text-white h-7 w-7"/>
+              < Twitter className="text-white h-7 w-7"/>
+              < Globe className="text-white h-7 w-7"/>
+
+            </div>
+
+            <div className=" h-full w-[70%] text-white flex gap-x-3 justify-center items-center " >
+              
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+              <div className="h-32 w-28 bg-yellow-200 rounded-md" >
+              </div>
+
+            </div>
+
+
+          </div> */}
+
+          {/* <div className=" h-[30%] w-full flex flex-row justify-center items-center ">
+
+
+          </div> */}
+          
         </div>
       </Tilt>
     </div>
